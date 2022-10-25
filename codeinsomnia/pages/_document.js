c56b4aa54cb8b2ff193f589f952d762fa18577f1
izzy417 <israeldail2@gmail.com>
2022-10-25 05:59:45 +0000

    added new font

--- codeinsomnia/pages/_document.js
+++ codeinsomnia/pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-                <link href="https://fonts.googleapis.com/css2?family=Edu+VIC+WA+NT+Beginner&display=swap" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css2?family=Teko&display=swap" rel="stylesheet" />
             </Head>
             <body>
                 <Main />

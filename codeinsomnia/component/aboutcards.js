e97519c5bde3d823ace7049cc632324884650b1c
Austan123 <austan.augustin1@gmail.com>
2022-11-04 03:53:32 +0000

    aboutpage

---
+++ codeinsomnia/component/aboutcards.js
@@ -0,0 +1,14 @@
+import Image from 'next/image';
+
+export default function Aboutcards({about}){
+    return (
+        <section>
+            <image src="" />
+            <div>
+                <h2>{job.title}</h2>
+                <p>{company}</p>
+                <p>{company}</p>
+            </div>
+        </section>
+    )
+}
--- codeinsomnia/pages/about.js
+++ codeinsomnia/pages/about.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Aboutcards from '../component/aboutcards';
 
 const About = () => {
   return (
@@ -12,6 +13,23 @@ const About = () => {
         <h1 className='title'>About</h1>
       </section>
 
+    </div>
+
+    <div className='container'> 
+    <div className='name-2'>
+      <h2>Austan Augustin</h2>
+      <p>CEO & Co-founder</p>
+      <button className='button'>
+        Contact 
+      </button>
+    </div>
+    <div className='name-1'>
+      <h2>Israel Dail</h2>
+      <p>CFO & Co-founder</p>
+      <button className='button'>
+        Contact 
+      </button>
+    </div>
     </div>
     </>
   )

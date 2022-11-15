b44d3909668061cfb81c25c802ffafc1e0f8408d
izzy417 <israeldail2@gmail.com>
2022-11-15 03:02:44 +0000

    border around contact page - needs more design

--- codeinsomnia/pages/contact.js
+++ codeinsomnia/pages/contact.js
@@ -7,15 +7,18 @@ const Contact = () => {
         <title>Code Insomnia | Contact</title>
       </Head>
       <form>
+        
         <section className='titlebox'>
           <h1 className='title'>Contact Us</h1>
         </section>
+        <div className="contact-page">
         <main className="contact-form">
           <input className="contact-input" type="text" placeholder="first name" />
           <input className="contact-input" type="text" placeholder="last name" />
           <input className="contact-input" type="text" placeholder="email" />
           <input className="contact-input" type="text" placeholder="phone number" />
         </main>
+        </div>
       </form>
     </>
   )

a3563877f2dcf5f8549fc94e00a029d01549ad01
izzy417 <israeldail2@gmail.com>
2022-11-22 23:53:40 +0000

    pop up for contact button

--- codeinsomnia/pages/about.js
+++ codeinsomnia/pages/about.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
-import Aboutcards from '../component/aboutcards';
+import Popup from 'reactjs-popup';
+
+
 
 const About = () => {
   return (
@@ -20,17 +22,21 @@ const About = () => {
           <h2>Austan Augustin</h2>
           <p>CEO & Founder</p>
           <p>An ambitious mind coupled with a charismatic personality</p>
-          <button className='button'>
-            Contact
-          </button>
+          <Popup trigger={<button className='button'>Contact</button>} position="bottom">
+            <div className='contact-popup'>
+              email: austan.augustin1@gmail.com
+            </div>
+          </Popup>
         </div>
         <div className='name-1'>
           <h2>Israel Dail</h2>
           <p>CTO & Founder</p>
           <p>Analytical-minded with a hint of motivated curiosity</p>
-          <button className='button'>
-            Contact
-          </button>
+          <Popup trigger={<button className='button'>Contact</button>} position="bottom">
+            <div className='contact-popup'>
+              email: israeldail2@gmail.com 
+            </div>
+          </Popup>
         </div>
       </div>
     </>

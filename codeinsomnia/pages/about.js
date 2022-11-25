adeb7701b846582a28dc38016dfe712f3fa0a173
izzy417 <israeldail2@gmail.com>
2022-11-25 03:20:24 +0000

    slight change to about

--- codeinsomnia/pages/about.js
+++ codeinsomnia/pages/about.js
@@ -18,35 +18,42 @@ const About = () => {
       </div>
 
       <div className='container'>
-        <div className='name-2'>
-          <h2>Austan Augustin</h2>
-          <p>CEO & Founder</p>
-          <p>An ambitious mind coupled with a charismatic personality</p>
-          <Popup trigger={<button className='button'>Contact</button>} position="bottom">
-            <div className='contact-popup'>
-              email: austan.augustin1@gmail.com
-            </div>
-          </Popup>
-        </div>
-        <div className='name-1'>
-          <h2>Israel Dail</h2>
-          <p>CTO & Founder</p>
-          <p>Analytical-minded with a hint of motivated curiosity</p>
-          <Popup trigger={<button className='button'>Contact</button>} position="bottom">
-            <div className='contact-popup'>
-              email: israeldail2@gmail.com 
-            </div>
-          </Popup>
-        </div>
-        <p className='paragraph'>Code-aire is led by two motivated people who share a dream. A dream of prosperity, freedom, and creativity. <br/> 
-        Our goal is to make web applications that can change the way people live on a daily basis and just make the world <br/>
-        a friendlier feeling place in general. We plan on acheiving this by releasing our own original products, the websites and <br/>
-        applications we make will be purely for the mutual satisfaction it will bring to both our team and <em>you</em> the general <br/>
-        population, more importantly our society.
-        <br/>
-        <br/>
-        <span className='company-founded'>Code-aire was founded in 2022, wait for big things ahead. </span>
-         </p>
+        <section className='card-container'>
+          <div className='name-2'>
+            <h2>Austan Augustin</h2>
+            <p>CEO & Founder</p>
+            <p>An ambitious mind coupled with a charismatic personality</p>
+            <Popup trigger={<button className='button'>Contact</button>} position="bottom">
+              <div className='contact-popup'>
+                email: austan.augustin1@gmail.com
+              </div>
+            </Popup>
+          </div>
+          <div className='name-1'>
+            <h2>Israel Dail</h2>
+            <p>CTO & Founder</p>
+            <p>Analytical-minded with a hint of motivated curiosity</p>
+            <Popup trigger={<button className='button'>Contact</button>} position="bottom">
+              <div className='contact-popup'>
+                email: israeldail2@gmail.com
+              </div>
+            </Popup>
+          </div>
+        </section>
+
+        <hr style={{ width: "95%" }} />
+
+        <h1 className='title'>Our Mission</h1>
+
+        <p className='paragraph'>Code-aire is led by two motivated people who share a dream. A dream of prosperity, freedom, and creativity. <br />
+          Our goal is to make web applications that can change the way people live on a daily basis and just make the world <br />
+          a friendlier feeling place in general. We plan on acheiving this by releasing our own original products, the websites and <br />
+          applications we make will be purely for the mutual satisfaction it will bring to both our team and <em>you</em> the general <br />
+          population, more importantly our society.
+          <br />
+          <br />
+          <span className='company-founded'>Code-aire was founded in 2022, wait for big things ahead. </span>
+        </p>
       </div>
     </>
   )
--- codeinsomnia/pages/services.js
+++ codeinsomnia/pages/services.js
@@ -17,62 +17,62 @@ const Services = () => {
         <section>
           <div className='html-stack'>
             <Image
-            src={'/html.png'}
-            width={500}
-            height={500}
-             />
-             <Image
-            src={'/java-script.png'}
-            width={500}
-            height={500}
-             />
-             <Image
-            src={'/css-logo.png'}
-            width={500}
-            height={500}
-             />
+              src={'/html.png'}
+              width={500}
+              height={500}
+            />
+            <Image
+              src={'/java-script.png'}
+              width={500}
+              height={500}
+            />
+            <Image
+              src={'/css-logo.png'}
+              width={500}
+              height={500}
+            />
           </div>
         </section>
 
         <section className='languages-container'>
           <div className='html-stack'>
             <Image
-            src={"/react-native-logo.png"}
-            height={500}
-            width={500}
+              src={"/react-native-logo.png"}
+              height={500}
+              width={500}
             />
 
             <Image
-            src={"/python-logo.png"}
-            height={500}
-            width={500}
+              src={"/python-logo.png"}
+              height={500}
+              width={500}
             />
 
           </div>
         </section>
 
-        <hr style={{width: "95%"}}/>
+        <hr style={{ width: "95%" }} />
 
         <section className='fade-in-text'>
           <h1 className='title'>Full Stack Development</h1>
-          <p className='paragraph-services'>Here at Code-aire we specialize in full stack development. We have growing experience with making websites and mobile apps <br/>
-          on front-end and back-end. Our passion and willigness to learn does not end there however, We have taken the initiative to <br/>
-          take on Python projects such as data analysis and automation. Our hard work is only matched by our passion that we bring to the table. <br/>
-          We will be showcasing all of our projects both client requested and our very own Code-aire projects in our portfolio section. <br/>
-          If you have not seen our portfolio page we highly encourage you to take a look <Link href="/portfolio"><a style={{color: "blue"}}>here</a></Link> </p>
+          <p className='paragraph-services'>Here at Code-aire we specialize in full stack development. We have growing experience with making websites and mobile apps <br />
+            on front-end and back-end. Our passion and willigness to learn does not end there however, We have taken the initiative to <br />
+            take on Python projects such as data analysis and automation. Our hard work is only matched by our passion that we bring to the table. <br />
+            We will be showcasing all of our projects both client requested and our very own Code-aire projects in our portfolio section. <br />
+            If you have not seen our portfolio page we highly encourage you to take a look <Link href="/portfolio"><a style={{ color: "blue" }}>here</a></Link> </p>
         </section>
 
-        <hr style={{width: "95%"}}/>
+        <hr style={{ width: "95%" }} />
 
         <section className='fade-in-text'>
           <h1 className='title'>Do you have an idea?</h1>
-          <p className='paragraph-services'>Code-aire will make them a reality for you, motivation and passion aside we are also about attitude.<br/>
-          We don't think in terms of <em>If's</em> or <em>But</em>, instead we say <strong>can</strong> and <strong>will</strong>. We don't believe in <br/>
-          doubting ourselves, therefore neither should our clients. If you have any projects or ideas you would like to bring our attention please fill out our <br/>
-          contact form <Link href="/contact"><a style={{color:"blue"}}>here</a></Link>
+          <p className='paragraph-services'>Code-aire will make them a reality for you, motivation and passion aside we are also about attitude.<br />
+            We don't think in terms of <em>If's</em> or <em>But</em>, instead we say <strong>can</strong> and <strong>will</strong>. We don't believe in <br />
+            doubting ourselves, therefore neither should our clients. If you have any projects or ideas you would like to bring our attention please fill out our <br />
+            contact form <Link href="/contact"><a style={{ color: "blue" }}>here</a></Link>
           </p>
         </section>
-        
+
       </div>
     </>
   )

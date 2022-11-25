06cafa039e3c2339748381903c66395aeaa3b4a7
izzy417 <israeldail2@gmail.com>
2022-11-25 14:40:01 +0000

    started backend for email contact form

--- codeinsomnia/pages/api/contact.js
+++ codeinsomnia/pages/api/contact.js
@@ -1,2 +1,30 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import { mailOptions, transporter } from "../../config/nodemailer";
+
+const handler = async (req, res) => {
+  
+  if(req.method === "POST") {
+    const data = req.body;
+    if(!data.name || !data.user_email || !data.phone_number || !data.msg){
+      return res.status(400).json({ message: 'bad request' })
+    }
+    try {
+      await transporter.sendMail({
+        ...mailOptions,
+        text: "This is test string",
+        html: "<h1>Test Title</h1><p>some body text</p>"
+      });
+
+      return res.status(200).json({ success: true });
+    }catch(error){
+      console.log(error)
+      return res.status(400).json({ message: error.message })
+    }
+  }
 
 
+  return res.status(400).json({ message: 'bad request' })
+}
+
+export default handler;
--- codeinsomnia/pages/contact.js
+++ codeinsomnia/pages/contact.js
@@ -1,6 +1,32 @@
 import Head from "next/head";
+import { useState } from "react";
+import { sendContactForm } from "../lib/api";
+
+const initValues = {name:"", user_email:"", phone_number:"", msg:""}
+
+const initState = {values: initValues}
 
 const Contact = () => {
+  const [state, setState] = useState(initState)
+
+  const {values, isLoading} = state
+  
+  const handleChange = ({target}) => setState((prev) => ({
+    ...prev,
+    values: {
+      ...prev.values,
+      [target.name]: target.value
+    },
+  }));
+
+  const onSubmit = async () => {
+    setState((prev) => ({
+      ...prev,
+    }));
+    await sendContactForm(values)
+  }
+
+
   return (
     <>
       <Head>
@@ -13,13 +39,18 @@ const Contact = () => {
         </section>
         <div className="contact-page">
         <main className="contact-form">
-          <input className="contact-input" type="text" placeholder="first name" required />
-          <input className="contact-input" type="text" placeholder="last name" required />
-          <input className="contact-input" type="text" placeholder="email" required />
-          <input className="contact-input" type="text" placeholder="phone number" required />
-          <textarea className="textarea" type="text" placeholder="type message here" required />
+          <input className="contact-input" type="text" placeholder="name" name="name" value={values.name} onChange={handleChange} required />
+          <input className="contact-input" type="text" placeholder="email" name="user_email" value={values.user_email} onChange={handleChange} required />
+          <input className="contact-input" type="text" placeholder="phone number" name="phone_number" value={values.phone_number} onChange={handleChange
+          } required />
+          <textarea className="textarea" type="text" placeholder="type message here" name="msg" value={values.msg} onChange={handleChange} required />
 
-          <button type="submit" className="button">Send Email</button>
+          <button 
+          type="submit" 
+          className="button" 
+          onClick={onSubmit}
+          >
+            Send Email</button>
         </main>
         </div>
       </form>
